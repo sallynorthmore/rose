105b5d52af73f03103b141ea9684ff241cc80277
Sally Northmore <sally.northmore@gmail.com>
2016-11-29 17:33:50 +0000

    Add 404 page

---
+++ wordpress/wp-content/themes/rosepilkington/404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+<h2>Not found...</h2>
+<p>
+	Oops, we cannot find your page. Try <a href="/">here</a>.
+</p>
+
+
+<?php get_footer(); ?>

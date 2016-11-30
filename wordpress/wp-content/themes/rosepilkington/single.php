8e719adeac5f04d0b5d2547ff062d877f02c365c
Sally Northmore <sally.northmore@gmail.com>
2016-11-30 12:15:47 +0000

    Start styling Project page content
    Create Project component CSS
    Update markup around content, meta data, title
    Style spacing and typography for mobile

--- wordpress/wp-content/themes/rosepilkington/single.php
+++ wordpress/wp-content/themes/rosepilkington/single.php
@@ -3,23 +3,24 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
 <section id="main" role="main" class="Project">
-
-	<div class="Project-button">
-		<div class="Close">
-			<a href="/archives/category/work" title="close" class="Close-inner">close</a>
+	<div class="Project-inner">
+		<div class="Project-button">
+			<div class="Close">
+				<a href="/archives/category/work" title="close" class="Close-inner">close</a>
+			</div>
 		</div>
-	</div>
 
-	<article id="post_<?php the_ID(); ?>" class="Project-content">
-
+		<div class="Project-client">
+			<?php the_meta(); ?>
+		</div>
 		<h1 class="Project-title">
 			<?php the_title(); ?>
 		</h1>
 
-		<div class="Project-body">
-			<?php the_content(); ?>
-		</div>
-	</article>
+		<article id="post_<?php the_ID(); ?>" class="Project-content">
+				<?php the_content(); ?>
+		</article>
+	</div>
 </section>
 <?php endwhile; ?>
 <?php endif; ?>

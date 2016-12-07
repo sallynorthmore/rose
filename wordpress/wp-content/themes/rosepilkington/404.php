20e5beefbafec6e5778efaaa3756dd3e9bcc75df
Sally Northmore <sally.northmore@gmail.com>
2016-12-07 16:13:46 +0000

    Style 404 page

--- wordpress/wp-content/themes/rosepilkington/404.php
+++ wordpress/wp-content/themes/rosepilkington/404.php
@@ -9,10 +9,19 @@
 
 get_header(); ?>
 
-<h2>Not found...</h2>
-<p>
-	Oops, we cannot find your page. Try <a href="/">here</a>.
-</p>
+<section id="main" role="main">
+	<div class="Page-inner">
+
+			<div class="Content">
+				<h1>
+					404 Not found...
+				</h1>
+				<p>
+					Oops, we cannot find your page. Try <a href="/">here</a>.
+				</p>
+			</div>
+	</div>
+</section>
 
 
 <?php get_footer(); ?>
